669ac6cb1561145a292e5c15c087c5d72b41ea04
Raymond Nijssen <r.nijssen@terglobo.nl>
2017-05-19 18:18:51 +0200

    renamed hxl_tools and added to makefile

--- Hxl/testfunc.py
+++ Hxl/testfunc.py
@@ -1,4 +1,4 @@
-from hxl_tools import HxlDataSet
+from hxl_manager import HxlDataSet
 import csv
 
 hxl_remote_json = 'https://proxy.hxlstandard.org/data.json?url=https%3A//data.humdata.org/dataset/3cb60971-0dc7-4743-a7ae-e65744b2dbba/resource/968202f1-856a-4906-ae87-c730e9b1dd27/download/PHL_haima_houses_damaged_pcoded_ndrrmc_sitrep_9_20161025.csv'
